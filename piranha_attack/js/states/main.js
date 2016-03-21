eeeb1a63de206ea0a3c576ba67ec643839ded760
ma3lstrom <sebastienluong@gmail.com>
2016-03-21 15:27:30 -0700

    Implement jump

--- piranha_attack/js/states/main.js
+++ piranha_attack/js/states/main.js
@@ -20,6 +20,11 @@ MAIN.createHelper = {
     item = startLand.createStructure();
     startLand.changeScale(item, 1 , 5);
 
+    var stopPoint = new Structure(400, game.world.height-25, MAIN.LAND_IMG,
+                                  platforms);
+    item = stopPoint.createStructure();
+    stopPoint.changeScale(item,1,1);
+
     var endLand = new Structure(game.world.width-300, game.world.height-150,
                             MAIN.LAND_IMG, platforms);
     item = endLand.createStructure();
@@ -30,18 +35,23 @@ MAIN.createHelper = {
     var player = new Player(game.world.width/2, game.world.height-50, 
                             MAIN.P_IMG);
     player = player.setupPlayer();
+    console.log(player);
     return player;
   },
 };
 
 MAIN.updateHelper = {
+  detectBottom: function(player, platforms){
+    game.physics.arcade.collide(player, platforms);
+  },
+
   detectJump: function(player, spaceKey){
-    // Jump
-    if (spaceKey.isDown){
+    // Jump when sprite is stationary
+    if (spaceKey.isDown && player.body.velocity.y === 0){
       if (player.alive === false){
         return;
       }
-      player.body.velocity.y = -350;
+      player.body.velocity.y = -100;
     }
   },
 };
@@ -55,6 +65,7 @@ var mainState = {
   }, 
 
   update:function() {
+    MAIN.updateHelper.detectBottom(MAIN.player, MAIN.platforms);
     MAIN.updateHelper.detectJump(MAIN.player, MAIN.spaceKey);
   }
 };
