437db9a3cd88d343903c210ede11e1cdc76949aa
ma3lstrom <sebastienluong@gmail.com>
2016-03-20 18:14:33 -0700

    Add keyboard cursor

--- piranha_attack/js/states/main.js
+++ piranha_attack/js/states/main.js
@@ -1,5 +1,17 @@
 
+var MAIN = {
+  P_IMG : 'player',
+  LAND_IMG : 'land'
+};
+
 var game = game;
+
+MAIN.createHelper = {
+  addKeyboard: function(){
+    var cursor = game.input.keyboard.createCursorKeys();
+    return cursor;
+  }
+};
 
 var mainState = {
   create:function() {
